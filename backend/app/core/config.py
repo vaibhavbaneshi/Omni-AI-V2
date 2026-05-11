ab113934c50dc02ef52f8c62bf43f419b6b9fb11
Vaibhav Baneshi <vaibhavbaneshi@Vaibhavs-MacBook-Air.local>
2026-05-11 10:05:35 +0530

    Updated the config and requirements.txt file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-
 import os
 
 load_dotenv()
@@ -8,13 +7,9 @@ class Settings:
 
     APP_NAME = os.getenv("APP_NAME")
 
-    OLLAMA_URL = os.getenv(
-        "OLLAMA_URL"
-    )
+    MODEL_NAME = os.getenv("MODEL_NAME")
 
-    MODEL_NAME = os.getenv(
-        "MODEL_NAME"
-    )
+    OLLAMA_URL = os.getenv("OLLAMA_URL")
 
     CHROMA_DB_PATH = os.getenv(
         "CHROMA_DB_PATH"
@@ -22,6 +17,26 @@ class Settings:
 
     COLLECTION_NAME = os.getenv(
         "COLLECTION_NAME"
+    )
+
+    POSTGRES_HOST = os.getenv(
+        "POSTGRES_HOST"
+    )
+
+    POSTGRES_PORT = os.getenv(
+        "POSTGRES_PORT"
+    )
+
+    POSTGRES_DB = os.getenv(
+        "POSTGRES_DB"
+    )
+
+    POSTGRES_USER = os.getenv(
+        "POSTGRES_USER"
+    )
+
+    POSTGRES_PASSWORD = os.getenv(
+        "POSTGRES_PASSWORD"
     )
 
 settings = Settings()
